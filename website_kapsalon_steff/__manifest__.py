848d36c0ff4a8f416ec6322dc041b40d2fe02481
ChristopheVdd <167391537+C-VERP@users.noreply.github.com>
2025-08-31 19:35:34 +0200

    Add files via upload

---
+++ website_kapsalon_steff/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website Kapsalon Steff',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Custom homepage for Kapsalon Steff',
+    'author': 'ChatGPT',
+    'depends': ['website'],
+    'data': [
+        'views/kapsalon_steff_homepage.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
